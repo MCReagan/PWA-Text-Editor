a36df1b0bdc224e07c46132fc10e5d3d9739ab1c
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-06-02 19:07:44 -0500

    get function implemented, will test

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
+async function getjateStore() {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  return store;
+};
+
+export const getDb = async () => {
+  const store = await getjateStore();
+  const result = await store.getAll();
+  return result;
+};
+
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
 
 initdb();
